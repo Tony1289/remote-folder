5f0a594daba42acfea0529589b3ec021ade94a56
Tony <tonytamilarasan@gmail.com>
2025-05-04 23:56:07 +0530

    committed

--- armstrong.py
+++ armstrong.py
@@ -1,7 +1,8 @@
-n = 1000
-while n>100:
-    result=int(str(n)[0])**3+int(str(n)[1])**3+int(str(n)[2])**3
-    if result==n:
-        print("The", n ,"is a armstrong number")
-    n -= 1
-    
+import hashlib  
+import os
+import random
+import string
+def generate_random_string(length=10):
+    """Generate a random string of fixed length """
+    letters = string.ascii_letters
+    return ''.join(random.choice(letters) for i in range(length))
